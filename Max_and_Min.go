5e204cfbb0305ab478ebd3d547a19bf93f467d8a
wqlparallel <1765347273@qq.com>
2022-03-22 10:40:35 +0800

    add Min and Max function

---
+++ Max_and_Min.go
@@ -0,0 +1,22 @@
+package pk_for_leetcode
+
+
+func Min(numbers ...int)int{
+	min := numbers[0]
+	for _, number := range numbers{
+		if number < min{
+			min = number
+		}
+	}
+	return min
+}
+
+func Max(numbers ...int)int{
+	max := numbers[0]
+	for _, number := range numbers{
+		if number > max{
+			max = number
+		}
+	}
+	return max
+}
